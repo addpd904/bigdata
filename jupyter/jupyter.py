cc3a65a4c7599359f81f03f9be20c1e33d322549
addpd904 <2056098523@qq.com>
2023-01-07 17:25:03 +0800

    numpy

--- jupyter/jupyter.py
+++ jupyter/jupyter.py
@@ -1,2 +1,10 @@
 # pip install -i https://pypi.tuna.tsinghua.edu.cn/simple notebook
 # pip install -i https://pypi.tuna.tsinghua.edu.cn/simple matplotlib
+import matplotlib.pyplot as plt
+
+# 一、basic grammer
+# plt.figure()
+#create a figure
+# x axis and y axis
+plt.plot([1,2,4,5],[5,9,4,6])
+plt.show()
